aea7577ceff16c02c92e52481ed47091b18ff70a
DhorajiyaGopal <23ceuof038@ddu.ac.in>
2025-07-08 04:43:02 +0000

    add prime function

--- Lab2/check_prime.cpp
+++ Lab2/check_prime.cpp
@@ -17,8 +17,46 @@ int power(ull a,ull n,ull p)
     }
     return res;
 }
+int gcd(int a,int b)
+{
+    int r;
+    while (b!=0)
+    {
+        r = a % b;
+        a = b ;
+        b = r;
+    }
+    return a;
+    
+}
+bool is_prime_randomized(ull n,int k)
+{
+    if(n <= 1 || n == 4)
+    {
+        return false;
+    }
+    if(n <= 3)
+    {
+        return true;
+    }
+    while(k > 0)
+    {
+        srand(time(0));
+        ull a = 2 + (rand() % (n - 4));
+        if(gcd(n,a) != 1)
+        {
+            return false;
+        }
+        if(power(a,n-1,n) != 1)
+        {
+            return false;
+        }
+        k--;
+    }
+    return true;
+}
 int main()
 {
-    cout<<power(2,10,100002);
+    cout<<(bool)is_prime_randomized(5,400);
     
 }
